7d280771a536bfe7ed007fed4ea85486c2ea8c4e
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2022-11-09 22:18:17 +0530

    Create WATERCONS.cpp

---
+++ codechef starters64/WATERCONS.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+#define ll long long
+
+void solve(){
+	ll n;
+	cin>>n;
+	if(n>=2000){
+		cout<<"YES\n";
+	}else{
+		cout<<"NO\n";
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+}
