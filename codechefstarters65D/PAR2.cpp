07e2763ce45b413366a8485588223ad138993123
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2022-11-16 20:33:34 +0530

    Create PAR2.cpp

---
+++ codechefstarters65D/PAR2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+void solve(){
+    int z;
+    cin>>z;
+    if(z&1){
+        cout<<"NO\n";
+    }else{
+        cout<<"YES\n";
+    }
+
+ }
+
+signed main()
+{
+     ios::sync_with_stdio(false);
+     cin.tie(NULL);
+     int t;
+     cin>>t;
+     while(t--){
+     solve();
+     }
+     return 0;
+}
