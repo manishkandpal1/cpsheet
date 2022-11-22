3384570f4a929ad87434ff9d2ddf388f52d493ce
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2022-11-22 20:33:46 +0530

    Create _10_1030A_In_Search_of_An_EasyProblem.cpp

---
+++ Implementation||constructive/_10_1030A_In_Search_of_An_EasyProblem.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+
+void solve(){
+    int t;
+    cin>>t;
+    int x;
+    for(int i=0;i<t;i++){
+        cin>>x;
+        if(x==1){
+            cout<<"HARD\n";
+            return;
+         }
+	}
+    cout<<"EASY\n";
+}
+
+
+signed main()
+{
+     ios::sync_with_stdio(false);
+     cin.tie(NULL);
+        solve();
+}
